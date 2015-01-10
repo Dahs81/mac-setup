d8cca397435695092e5efd44e919eb209afcb3ef
Shad Beard <sbeard@ensequence.com>
2015-01-09 23:17:49 -0500

    update script to make more modular

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Setup xcode tools
- if ! pkgutil --pkg-info=com.apple.pkg.CLTools_Executables > /dev/null 2>&1; then
-   echo "xcode-select --install"
- else
-   echo "xcode tools already installed"
- fi
+# Todo: make each install a function and a main() that calls the functions
 
-# Setup node.js
+
+# Setup node.js/nvm
 
 
 # Setup go/gvm
@@ -16,4 +12,22 @@
 # Setup .aliases
 
 
+
 #  Setup .profile
+
+# Setup xcode tools
+function install_xcode_tools
+{
+  if ! pkgutil --pkg-info=com.apple.pkg.CLTools_Executables > /dev/null 2>&1; then
+    echo "xcode-select --install"
+  else
+    echo "xcode tools already installed"
+  fi
+}
+
+function main
+{
+  install_xcode_tools
+}
+
+main
